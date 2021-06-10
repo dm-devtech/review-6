8763bc6dd81c75d1202ffc9ac90e6c4f4dd4c7e4
David Millen <70208036+dm-devtech@users.noreply.github.com>
2021-06-10 11:24:23 +0100

    feat: added calculation of results

--- src/schoolReport.js
+++ src/schoolReport.js
@@ -1,7 +1,24 @@
 class SchoolReport {
 
-  result() {
-    return "Green: 2\nAmber: 1\nRed: 3"
+  result(marks) {
+    const results = marks.split(",")
+    let green = 0
+    let amber = 0
+    let red = 0
+
+    for(let i = 0; i < results.length; i++){
+      if(results[i] >= 75){
+        green += 1
+      } else if (results[i] >= 50 && results[i] <= 74){
+        amber += 1
+      } else if (results[i] <= 49) {
+        red += 1
+      }
+    }
+
+    const colourResults = `Green: ${green}\nAmber: ${amber}\nRed: ${red}`
+
+    return colourResults
   }
 
 }
