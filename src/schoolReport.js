b94f575f183d7e27499fff4c43ade4d945fbb2e0
David Millen <70208036+dm-devtech@users.noreply.github.com>
2021-06-10 11:44:21 +0100

    feat: zero results dont appear

--- src/schoolReport.js
+++ src/schoolReport.js
@@ -5,16 +5,23 @@ class SchoolReport {
     let green = 0
     let amber = 0
     let red = 0
+    let uncounted = 0
 
     for(let i = 0; i < results.length; i++){
-      if (results[i] >= 75) green += 1
+      if (results[i] < 0 || results[i] > 100) uncounted += 1
+      if (results[i] >= 75 && results[i] <= 100) green += 1
       if (results[i] >= 50 && results[i] <= 74) amber += 1
-      if (results[i] <= 49) red += 1
+      if (results[i] <= 49 && results[i] >= 0) red += 1
     }
 
-    const colourResults = `Green: ${green}\nAmber: ${amber}\nRed: ${red}`
+    const colourResults = []
 
-    return colourResults
+    if (green > 0) colourResults.push(`Green: ${green}`)
+    if (amber > 0) colourResults.push(`Amber: ${amber}`)
+    if (red > 0) colourResults.push(`Red: ${red}`)
+    if (uncounted > 0) colourResults.push(`Uncounted: ${uncounted}`)
+
+    return colourResults.join("\n")
   }
 
 }
--- src/schoolReport.test.js
+++ src/schoolReport.test.js
@@ -9,3 +9,13 @@ test('test output with different results', () => {
   const report = new SchoolReport
   expect(report.result("1,10,5,5,5,45,50,60,80,100,75,80")).toEqual("Green: 4\nAmber: 2\nRed: 6")
 })
+
+test('edge case of score above 100 and below 0', () => {
+  const report = new SchoolReport
+  expect(report.result("-5, 150, 50, 80")).toEqual("Green: 1\nAmber: 1\nUncounted: 2")
+})
+
+test('edge case of score above 100 and below 0', () => {
+  const report = new SchoolReport
+  expect(report.result("50")).toEqual("Amber: 1")
+})
