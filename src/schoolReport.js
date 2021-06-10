3535fa5a433b671cf2fc77d7c36cf07df81f0518
David Millen <70208036+dm-devtech@users.noreply.github.com>
2021-06-10 11:28:03 +0100

    test: additional results test added

--- src/schoolReport.js
+++ src/schoolReport.js
@@ -7,13 +7,9 @@ class SchoolReport {
     let red = 0
 
     for(let i = 0; i < results.length; i++){
-      if(results[i] >= 75){
-        green += 1
-      } else if (results[i] >= 50 && results[i] <= 74){
-        amber += 1
-      } else if (results[i] <= 49) {
-        red += 1
-      }
+      if (results[i] >= 75) green += 1
+      if (results[i] >= 50 && results[i] <= 74) amber += 1
+      if (results[i] <= 49) red += 1
     }
 
     const colourResults = `Green: ${green}\nAmber: ${amber}\nRed: ${red}`
--- src/schoolReport.test.js
+++ src/schoolReport.test.js
@@ -1,6 +1,11 @@
 import SchoolReport from './schoolReport'
 
-test('test output', () => {
+test('test output as per example', () => {
   const report = new SchoolReport
   expect(report.result("1,10,45,60,80,100")).toEqual("Green: 2\nAmber: 1\nRed: 3")
+})
+
+test('test output with different results', () => {
+  const report = new SchoolReport
+  expect(report.result("1,10,5,5,5,45,50,60,80,100,75,80")).toEqual("Green: 4\nAmber: 2\nRed: 6")
 })
